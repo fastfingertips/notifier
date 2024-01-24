23a1212915a0f80d6ab0bb7e050242a7dad7968f
fastfingertips <fastfingertips@gmail.com>
2024-01-25 02:00:32 +0300

    Add function descriptions to setup.py

--- modules/setup.py
+++ modules/setup.py
@@ -2,17 +2,20 @@ from . import os
 
 while True:
     try:
-       from win11toast import toast
-       break
+        from win11toast import toast
+        break
     except ImportError as ie:
         try: 
-           os.system(' && '.join([
-           'pip install pipreqs',
-           'pipreqs --encoding utf-8 --force',
-           'pip install -r requirements.txt'
-           ]))
+            # Installing pipreqs to generate requirements.txt
+            # and then installing required libraries
+            os.system(' && '.join([
+                'pip install pipreqs',
+                'pipreqs --encoding utf-8 --force',
+                'pip install -r requirements.txt'
+            ]))
         except Exception as e: 
-          print(F'Import Error: {ie}, Exception: {e}')
-          exit()
+            print(f'Import Error: {ie}, Exception: {e}')
+            exit()
 
+# Defining the list of modules to be exported
 __all__ = ['os', 'toast']
