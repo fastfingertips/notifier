b72f1049df8341c3289c0aee5d2800bb36126a97
fastfingertips <fastfingertips@gmail.com>
2024-01-26 22:48:36 +0300

    Reorganized modules folder for better accessibility using __init__.py.

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,4 +1,20 @@
+from .config import (
+  Constants,
+  Config
+)
+
+from datetime import datetime
+import pathlib
+import random
+import time
 import json
+import sys
 import os
 
+# .setup:req:os
 from .setup import toast
+
+# .errors:req:Constants
+from .errors import InvalidSyntax 
+from .errors import InvalidInputError
+from .errors import InvalidArg
