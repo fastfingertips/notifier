60640a8fc2c1bfade32f7d4dc764235be850c944
fastfingertips <fastfingertips@gmail.com>
2024-01-26 22:44:46 +0300

    Created new error classes.

--- modules/errors.py
+++ modules/errors.py
@@ -1,3 +1,21 @@
+from . import Constants
+
 class InvalidInputError(Exception):
     """Raised when the input format is invalid."""
-    pass
+    def __init__(self, msg):
+        print(msg)
+        print('Valid extensions:', ','.join(Constants.FILE_FORMATS))
+        exit()
+
+class InvalidSyntax(Exception):
+
+    def __init__(self, msg):
+        print(msg)
+        print('Please check file content.')
+        exit()
+
+class InvalidArg(Exception):
+
+    def __init__(self, msg):
+        print(msg)
+        exit()
